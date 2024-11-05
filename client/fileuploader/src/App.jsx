8dbf3c78e7865eeb4a21f7495db4f6f476a1b7d6
abishkar rai <raiabishkar0.5@gmail.com>
2024-11-05 16:09:23 +1100

    updated app.jsx file

--- client/fileuploader/src/App.jsx
+++ client/fileuploader/src/App.jsx
@@ -33,41 +33,34 @@ function App() {
 
     return (
         <div className='font-nunito'>
-            <Container>
-                <section>
-                    <div className='parent-container'>
-                        <h1 className='text-xl font-semibold p-4'>Data Type Inference</h1>
-                        <Form onSubmit={handleSubmit} className='img-upload d-flex align-items-center'>
-                            <Form.Group className='me-2'>
-                                <Form.Control
-                                    type="file"
-                                    onChange={handleFileChange}
-                                    accept=".csv, .xlsx"
-                                    className='input-content'
-                                />
-                            </Form.Group>
-                            <Button type="submit" className='p-1 text-xm font-semibold button-text'>
-                                Upload and Process
-                            </Button>
-                        </Form>
-                        <div>
-                            {error && <p style={{ color: 'red' }}>{error}</p>}
-                            {dataTypes && (
-                                <div>
-                                    <h2>Inferred Data Types</h2>
-                                    <ul>
-                                        {Object.entries(dataTypes).map(([column, dtype]) => (
-                                            <li key={column}>
-                                                <strong>{column}</strong>: {dtype}
-                                            </li>
-                                        ))}
-                                    </ul>
-                                </div>
-                            )}
-                        </div>
-                    </div>
-                </section>
-            </Container>
+        <Container>
+        <section>
+        <div className='parent-container'>
+         <h1 className='text-xl font-semibold p-4'>Data Type Inference</h1>
+            <Form onSubmit={handleSubmit} className='img-upload d-flex align-items-center'>
+                <Form.Group className='me-2'>
+                 <Form.Control type="file" onChange={handleFileChange} accept=".csv,.exel" className='input-content'/>
+                 </Form.Group>
+                 <Button type="submit" className='p-1 text-xm font-semibold button-text'>
+                  Upload and Process
+                  </Button>
+             </Form>
+            <div>
+                {error && <p style={{ color: 'red' }}>{error}</p>}
+                {dataTypes && (
+                <div>
+                <h2>Inferred Data Types</h2>
+                <ul>
+                {Object.entries(dataTypes).map(([column, dtype]) => (
+                <li key={column}>
+                <strong>{column}</strong>: {dtype}
+                </li>))}</ul>
+                </div>
+                 )}
+            </div>
+        </div>
+        </section>
+        </Container>
         </div>
     );
 }
